b13f8319336f0de2e7e1aa2323e6857e6baa82c0
halweg <halweg@126.com>
2021-12-29 22:22:32 +0800

    feat: add selection sort func

---
+++ basic_sort/selection_sort.go
@@ -0,0 +1,15 @@
+package basic_sort
+
+
+func SelectionSort(arr []int) []int {
+	for i:=0; i < len(arr); i++ {
+		minIndex := i
+		for j := i; j < len(arr); j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+	return arr
+}
