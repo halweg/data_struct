dbfd0ccb1eb46130188a33f02c0364a2d630c933
halweg <halweg@126.com>
2021-12-30 03:37:35 +0800

    no message

---
+++ array/dynamic_arr.go
@@ -0,0 +1,104 @@
+package array
+
+import (
+	"errors"
+	"fmt"
+)
+
+const OutOfRangeByUpdate = "index error, the index must < array length and > 0"
+const OutOfRangeByRead = "index error, the index must <= array length and > 0"
+
+type Array struct {
+	size int
+	Data [1000]int
+}
+
+func (a *Array) GetSize() int {
+	return a.size
+}
+
+func (a *Array) IsEmpty() bool {
+	return a.size == 0
+}
+
+func (a *Array) Add(index int, value int) error {
+	if index < 0 || index > a.size {
+		return errors.New(OutOfRangeByUpdate)
+	}
+	for i := a.size - 1; i >= index; i-- {
+		a.Data[i + 1] = a.Data[i]
+	}
+	a.Data[index] = value
+	a.size++
+	fmt.Println(a.Data)
+	return nil
+}
+
+func (a *Array) AddFirst(value int) error {
+	return a.Add(0, value)
+}
+
+func (a * Array) AddLast(value int) error {
+	return a.Add(a.size-1, value)
+}
+
+func (a *Array) Set(index int, value int) error {
+	if index < 0 || index >= a.size {
+		return errors.New(OutOfRangeByRead)
+	}
+	a.Data[index] = value
+	return nil
+}
+
+func (a *Array) Get(index int) (err error, res int) {
+	if index < 0 || index >= a.size {
+		return errors.New(OutOfRangeByRead), res
+	}
+	return err, a.Data[index]
+}
+
+func (a *Array) Remove(index int) (err error, res int) {
+	if index < 0 || index >= a.size {
+		return errors.New(OutOfRangeByRead), res
+	}
+	res = a.Data[index]
+	for i := a.size - 1; i >= index; i-- {
+		a.Data[i-1] = a.Data[i]
+	}
+	a.size--
+	return nil, res
+}
+
+func (a *Array) RemoveElement(value int) error {
+	_, index := a.Find(value)
+	if index != -1 {
+		_, _ = a.Remove(index)
+	}
+	return nil
+}
+
+func (a *Array) RemoveFirst() (err error, res int) {
+	return a.Remove(0)
+}
+
+func (a *Array) RemoveLast() (err error, res int) {
+	return a.Remove(a.size-1)
+}
+
+func (a Array) Find(value int) (err error, res int) {
+	for i := 0; i <= a.size; i++ {
+		if value == a.Data[i] {
+			return nil, i
+		}
+	}
+	return errors.New("not find"), -1
+}
+
+func NewArray() *Array {
+	var data [1000]int
+	arr := &Array{
+		size: 0,
+		Data: data,
+	}
+	return arr
+}
